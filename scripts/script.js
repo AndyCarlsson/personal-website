d24cf5eb16e6309d3831fa2cbfe1929f545e8e8a
AndyCarlsson <andreas95carlsson@gmail.com>
2020-06-21 17:21:46 +0200

    Removed unused css and added link to github

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,22 @@ $('.navbar a').on('click', function (e) {
       },800);
   }
 });
+
+
+//Add fade in on text
+$(document).on("scroll", function () {
+  var pageTop = $(document).scrollTop()
+  var pageBottom = pageTop + $(window).height()
+
+  var tags = $(".fade")
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i]
+
+    if ($(tag).position().top < pageBottom) { 
+      $(tag).addClass("visible")
+    }else {
+      $(tag).removeClass("visible")
+    }
+  }
+})
